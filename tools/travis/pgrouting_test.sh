644a4eee2238d92f9a3e166d831da857f76bb5d2
Daniel Kastl <daniel@georepublic.de>
2013-06-19 01:50:18 +0900

    Imported Upstream version 2.0.0-beta

--- tools/travis/pgrouting_test.sh
+++ tools/travis/pgrouting_test.sh
@@ -12,54 +12,76 @@ DBNAME="pgrouting"
 POSTGRESQL_VERSION="$1"
 POSTGIS_VERSION="$2"
 
+POSTGRESQL_DIRECTORY="/usr/share/postgresql/$POSTGRESQL_VERSION"
+
+# exit script on error
+set -e 
+ERROR=0
+
 # Define alias function for psql command
 run_psql () {
     PGOPTIONS='--client-min-messages=warning' psql -X -q -v ON_ERROR_STOP=1 --pset pager=off "$@"
+    if [ "$?" -ne 0 ]
+    then 
+        echo "Test query failed: $@"
+        ERROR=1
+    fi 
 }
 
 # ------------------------------------------------------------------------------
 # CREATE DATABASE
 # ------------------------------------------------------------------------------
-createdb -U postgres $DBNAME
+run_psql -U $DBUSER -l
+run_psql -U $DBUSER -c "CREATE DATABASE $DBNAME;"
 
 # ------------------------------------------------------------------------------
 # CREATE EXTENSION
 # ------------------------------------------------------------------------------
 if [ "$POSTGRESQL_VERSION" == "8.4" ] || [ "$POSTGRESQL_VERSION" == "9.0" ]
 then
-    run_psql -U postgres -d $DBNAME -f /usr/share/postgresql/$POSTGRESQL_VERSION/contrib/postgis-$POSTGIS_VERSION/postgis.sql
-    run_psql -U postgres -d $DBNAME -f /usr/share/postgresql/$POSTGRESQL_VERSION/contrib/postgis-$POSTGIS_VERSION/spatial_ref_sys.sql
-    run_psql -U postgres -d $DBNAME -f `find /usr/share -name "pgrouting--*.sql"`
+    run_psql -U $DBUSER -d $DBNAME -f `find $POSTGRESQL_DIRECTORY/contrib -name "postgis.sql"`
+    run_psql -U $DBUSER -d $DBNAME -f `find $POSTGRESQL_DIRECTORY/contrib -name "spatial_ref_sys.sql"`
+    run_psql -U $DBUSER -d $DBNAME -f `find $POSTGRESQL_DIRECTORY/contrib -name "pgrouting.sql"`
 fi
 
 if [ "$POSTGRESQL_VERSION" == "9.1" ]
 then
     if [ "$POSTGIS_VERSION" == "1.5" ]
     then 
-        run_psql -U postgres -d $DBNAME -f /usr/share/postgresql/$POSTGRESQL_VERSION/contrib/postgis-$POSTGIS_VERSION/postgis.sql
-        run_psql -U postgres -d $DBNAME -f /usr/share/postgresql/$POSTGRESQL_VERSION/contrib/postgis-$POSTGIS_VERSION/spatial_ref_sys.sql
-        run_psql -U postgres -d $DBNAME -f `find /usr/share -name "pgrouting--*.sql"`
+        run_psql -U $DBUSER -d $DBNAME -f `find $POSTGRESQL_DIRECTORY/contrib -name "postgis.sql"`
+        run_psql -U $DBUSER -d $DBNAME -f `find $POSTGRESQL_DIRECTORY/contrib -name "spatial_ref_sys.sql"`
+        run_psql -U $DBUSER -d $DBNAME -f `find $POSTGRESQL_DIRECTORY/contrib -name "pgrouting.sql"`
     else
-        run_psql -U postgres -d $DBNAME -c "CREATE EXTENSION postgis;"
-        run_psql -U postgres -d $DBNAME -c "CREATE EXTENSION pgrouting;"
+        run_psql -U $DBUSER -d $DBNAME -c "CREATE EXTENSION postgis;"
+        run_psql -U $DBUSER -d $DBNAME -c "CREATE EXTENSION pgrouting;"
     fi
 fi
 
 if [ "$POSTGRESQL_VERSION" == "9.2" ] || [ "$POSTGRESQL_VERSION" == "9.3" ]
 then
-    run_psql -U postgres -d $DBNAME -c "CREATE EXTENSION postgis;"
-    run_psql -U postgres -d $DBNAME -c "CREATE EXTENSION pgrouting;"
+    run_psql -U $DBUSER -d $DBNAME -c "CREATE EXTENSION postgis;"
+    run_psql -U $DBUSER -d $DBNAME -c "CREATE EXTENSION pgrouting;"
 fi
 
 # ------------------------------------------------------------------------------
 # Get version information
 # ------------------------------------------------------------------------------
-run_psql -U postgres -d $DBNAME -c "SELECT postgis_full_version();"
-run_psql -U postgres -d $DBNAME -c "SELECT pgr_version();"
+run_psql -U $DBUSER -d $DBNAME -c "SELECT postgis_full_version();"    
+run_psql -U $DBUSER -d $DBNAME -c "SELECT pgr_version();"
 
-PGROUTING_VERSION=`run_psql -U postgres -A -t -d $DBNAME -c "SELECT version FROM pgr_version();"`
+PGROUTING_VERSION=`run_psql -U $DBUSER -A -t -d $DBNAME -c "SELECT version FROM pgr_version();"`
 
 # ------------------------------------------------------------------------------
 # Test runner
 # ------------------------------------------------------------------------------
-./tools/test-runner.pl -pgver $POSTGRESQL_VERSION -pgisver $POSTGIS_VERSION -pgrver $PGROUTING_VERSION
+# use -v -v for more verbose debuging output
+# ./tools/test-runner.pl -v -v -pgver $POSTGRESQL_VERSION
+./tools/test-runner.pl -pgver $POSTGRESQL_VERSION
+if [ "$?" -ne 0 ]
+then
+    ERROR=1
+fi
+
+# Return success or failure
+# ------------------------------------------------------------------------------
+exit $ERROR
